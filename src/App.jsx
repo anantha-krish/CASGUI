db45e09779682f8b35f6dcee0a96240764ac369c
Anantha Krishnan <anantha.krishnan@ibsplc.com>
2020-10-15 11:15:03 +0530

    Committing all styles for topbar & sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import "./assets/sass/common.scss";
 import Topbar from "./components/common/topbar/Topbar";
 import Dashboard from "./components/dashboard/Dashboard";
+import classNames from 'classnames';
+import 'primereact/resources/themes/nova/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
 
 function App() {
+  const wrapperClass = classNames('layout-wrapper layout-static');
+
+const sidebarClassName = classNames("layout-sidebar");
   return (
-    <div className="App">
+    <div className={wrapperClass}>
       <Topbar/>
+      <div className="layout-main">
       <Dashboard/>
+      </div>
     </div>
   );
 }
--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -3,12 +3,7 @@ import React from 'react'
 function Dashboard() {
     return (
         <div className="dashboard-wrapper">
-        {/* grid test code */}
-        <div className="p-grid grid-test">{/*  remove grid-test class in future */}
-            <div className="p-col-2">col 2 of 12</div>
-            <div className="p-col-4">col 4 of 12 </div>
-            <div className="p-col-6">col 6 of 12 </div>
-        </div>
+     
         </div>
     )
 }
