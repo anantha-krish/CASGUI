03787cc56361fa8bda4f80de7d59691e0d41e3f5
Anantha Krishnan <anantha.krishnan@ibsplc.com>
2020-10-16 17:58:35 +0530

    class conversion top & dashboard

--- src/components/common/topbar/CasTopbar.jsx
+++ src/components/common/topbar/CasTopbar.jsx
@@ -1,37 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 import { InputText } from "primereact/inputtext";
 import PropTypes from "prop-types";
 
-function CasTopbar(props) {
-  return (
-    <div className="layout-topbar clearfix">
-      <button
-        className="p-link layout-menu-button"
-        onClick={props.onToggleMenu}
-      >
-        <span className="pi pi-bars" />
-      </button>
-
-      <div className="layout-topbar-icons">
-        <span className="layout-topbar-search">
-          <InputText type="text" placeholder="Search" />
-          <span className="layout-topbar-search-icon pi pi-search" />
-        </span>
-        <button className="p-link">
-          <span className="layout-topbar-item-text">Alerts</span>
-          <span className="layout-topbar-icon pi pi-envelope" />
-          <span className="layout-topbar-badge">5</span>
+class CasTopbar extends Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+    return (
+      <div className="layout-topbar clearfix">
+        <button
+          className="p-link layout-menu-button"
+          onClick={this.props.onToggleMenu}
+        >
+          <span className="pi pi-bars" />
         </button>
-        <button className="p-link">
-         
-        <span className="layout-topbar-icon user-icon" > <img src="assets/images/profile.png" alt="" /> </span>
-         
-          <span className="layout-topbar-username">Taro Sorano</span>
-        </button>
+        <div className="layout-topbar-icons">
+          <span className="layout-topbar-search">
+            <InputText type="text" placeholder="Search" />
+            <span className="layout-topbar-search-icon pi pi-search" />
+          </span>
+          <button className="p-link">
+            <span className="layout-topbar-item-text">Alerts</span>
+            <span className="layout-topbar-icon pi pi-envelope" />
+            <span className="layout-topbar-badge">5</span>
+          </button>
+          <button className="p-link">
+            <span className="layout-topbar-icon user-icon">
+              <img src="assets/images/profile.png" alt="User Profile" />
+            </span>
+            <span className="layout-topbar-username">Taro Sorano</span>
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
 CasTopbar.defaultProps = {
   onToggleMenu: null,
 };
--- src/components/dashboard/CasDashboard.jsx
+++ src/components/dashboard/CasDashboard.jsx
@@ -1,70 +1,90 @@
-import React, { useState, useEffect } from "react";
+import React, { Component } from "react";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import { InputText } from "primereact/inputtext";
-import { UserService } from '../../service/services';
+import { UserService } from "../../service/services";
 
-function CasDashboard() {
-  const [userList, setuserList] = useState([]);
-  const [selectedUser, setselectedUser] = useState("");
-  const [globalFilter, setglobalFilter] = useState("");
+class CasDashboard extends Component {
+  constructor() {
+    super();
 
-  function setUserDetailsCallBack(data){
-    setuserList(data);
+    this.state = {
+      userList: [],
+      selectedUser: "",
+      globalFilter: "",
+    };
+    this.setUserDetailsCallBack = this.setUserDetailsCallBack.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  useEffect(() => {
-   UserService.getUsers(setUserDetailsCallBack);
-  }, []);
+  setUserDetailsCallBack(userListJson) {
+    this.setState({
+      userList: userListJson,
+    });
+  }
 
-  return (
-    <div className="p-grid">
-      <div className="p-col-12">
-        <div className="card card-w-title">
-          <div className="p-grid">
-            <div className="p-col-9">
-              {" "}
-              <h1>
-                {selectedUser
-                  ? "User -> " + selectedUser.name
-                  : "No User selected"}
-              </h1>
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  componentDidMount() {
+    UserService.getUsers(this.setUserDetailsCallBack);
+  }
+
+  render() {
+    return (
+      <div className="p-grid">
+        <div className="p-col-12">
+          <div className="card card-w-title">
+            <div className="p-grid">
+              <div className="p-col-9">
+                <h1>
+                  {this.state.selectedUser
+                    ? "User -> " + this.state.selectedUser.name
+                    : "No User selected"}
+                </h1>
+              </div>
+              <div className="p-col">
+                <span className="p-input-icon-right">
+                  <i className="pi pi-search" />
+                  <InputText
+                    type="search"
+                    onInput={this.handleChange}
+                    name="globalFilter"
+                    placeholder="Search"
+                    size="30"
+                  />
+                </span>
+              </div>
             </div>
-            <div className="p-col">
-              <span className="p-input-icon-right">
-                <i className="pi pi-search" />
-                <InputText
-                  type="search"
-                  onInput={(e) => setglobalFilter(e.target.value)}
-                  placeholder="Search"
-                  size="30"
-                />
-              </span>
-            </div>
+
+            <DataTable
+              value={this.state.userList}
+              selectionMode="single"
+              selection={this.state.selectedUser}
+              onSelectionChange={(event) =>
+                this.setState({ selectedUser: event.value })
+              }
+              paginator
+              rows={5}
+              paginatorPosition="both"
+              responsive
+              globalFilter={this.state.globalFilter}
+            >
+              <Column field="id" header="User Id" sortable />
+              <Column field="username" header="User Name" sortable />
+              <Column field="name" header="Full Name" sortable />
+              <Column field="email" header="Email Id" sortable />
+              <Column field="address.street" header="Street" sortable />
+              <Column field="company.name" header="Company" sortable />
+            </DataTable>
           </div>
-
-          <DataTable
-            value={userList}
-            selectionMode="single"
-            selection={selectedUser}
-            onSelectionChange={(event) => setselectedUser(event.value)}
-            paginator
-            rows={5}
-            paginatorPosition="both"
-            responsive
-            globalFilter={globalFilter}
-          >
-            <Column field="id" header="User Id" sortable />
-            <Column field="username" header="User Name" sortable />
-            <Column field="name" header="Full Name" sortable />
-            <Column field="email" header="Email Id" sortable />
-            <Column field="address.street" header="Street" sortable />
-            <Column field="company.name" header="Company" sortable />
-          </DataTable>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default CasDashboard;
