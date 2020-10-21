dc1793c1d1bb93f38dd10906202edef93e632cfc
MarvelPOC <68634759+MarvelPOC@users.noreply.github.com>
2020-10-21 09:23:56 +0530

    Update CasTopbar.jsx

--- src/components/common/topbar/CasTopbar.jsx
+++ src/components/common/topbar/CasTopbar.jsx
@@ -13,29 +13,38 @@ class CasTopbar extends Component {
   };
   
   render() {
+    let menuToggleClassName ="pi pi-chevron-left";
+    if(this.props.staticMenuInactive){
+      menuToggleClassName ="pi pi-chevron-right";
+    }
     return (
-      <div className="layout-topbar clearfix">
-        <button
-          className="p-link layout-menu-button"
-          onClick={this.props.onToggleMenu}>
-          <span className="pi pi-bars" />
-        </button>
-        <div className="layout-topbar-icons">
-          <span className="layout-topbar-search">
-            <InputText type="text" placeholder="Search" />
-            <span className="layout-topbar-search-icon pi pi-search" />
-          </span>
-          <button className="p-link">
-            <span className="layout-topbar-item-text">Alerts</span>
-            <span className="layout-topbar-icon pi pi-envelope" />
-            <span className="layout-topbar-badge">5</span>
+      <div>
+        <div className="layout-topbar-left clearfix">
+          <span className="layout-topbar-logo-name">CAS GUI</span>
+          <button
+            className="p-link layout-menu-button"
+            onClick={this.props.onToggleMenu}>
+            <span className={menuToggleClassName} />
           </button>
-          <button className="p-link">
-            <span className="layout-topbar-icon user-icon">
-              <img src={require('../../../assets/images/profile.png') } alt="User Profile" />
+        </div>
+        <div className="layout-topbar clearfix">
+          <div className="layout-topbar-icons">
+            <span className="layout-topbar-search">
+              <InputText type="text" placeholder="Search" />
+              <span className="layout-topbar-search-icon pi pi-search" />
             </span>
-            <span className="layout-topbar-username">Taro Sorano</span>
-          </button>
+            <button className="p-link">
+              <span className="layout-topbar-item-text">Alerts</span>
+              <span className="layout-topbar-icon pi pi-envelope" />
+              <span className="layout-topbar-badge">5</span>
+            </button>
+            <button className="p-link">
+              <span className="layout-topbar-icon user-icon">
+                <img src={require('../../../assets/images/profile.png') } alt="User Profile" />
+              </span>
+              <span className="layout-topbar-username">Taro Sorano</span>
+            </button>
+          </div>
         </div>
       </div>
     );
