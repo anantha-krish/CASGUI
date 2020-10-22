48e28634e7dd87f3142724a70919d8aeaa27a0db
Anantha Krishnan <anantha.krishnan@ibsplc.com>
2020-10-22 20:42:22 +0530

    TimePicker added

---
+++ src/components/common/formfields/CasTimePicker.jsx
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import TimeField from 'react-simple-timefield';
+import CasInputText from './CasInputText';
+
+class CasTimePicker extends Component {
+    
+    static propTypes = {
+        id:PropTypes.string,
+        onChange:PropTypes.func.isRequired,
+        name:PropTypes.string.isRequired,
+        value:PropTypes.any,
+        disabled:PropTypes.bool,
+        errorText:PropTypes.string,
+        placeholder:PropTypes.string,
+        className: PropTypes.string,
+
+    }
+
+    static defaultProps = {
+        disabled: false,
+        id:'',
+        name:'',
+        value:'',
+        errorText:"",
+    };
+
+ 
+    renderErrorText() {
+      if(this.props.errorText){
+        return(
+          <small className="cas-inline-err-text p-d-block">{this.props.errorText}</small>
+        );
+      }
+    }
+
+    renderField() {
+      const {
+        id,
+        onChange,
+        name,
+        value,
+
+        
+      } = this.props;
+      let className = this.props.className;
+      if(this.props.errorText){
+        className = className + " p-invalid";
+      }
+      return (
+        <TimeField
+        id={id}
+        name={name}
+    value={value}                     // {String}   required, format '00:00' or '00:00:00'
+    onChange={onChange}      // {Function} required
+    input={<CasInputText />} // {Element}  default: <input type="text" />
+    colon=":"                        // {String}   default: ":"
+                         // {Boolean}  default: false
+/>
+      )
+
+    }
+
+    render () {
+      return (
+        <div className="cas-input-field">
+          {this.renderField()}
+          {this.renderErrorText()}
+        </div>
+      )
+    }
+
+  }
+
+export default CasTimePicker;
--- src/components/dashboard/CasFormDemo.jsx
+++ src/components/dashboard/CasFormDemo.jsx
@@ -8,27 +8,27 @@ import { Formik } from "formik";
 import { AirportService } from "../../service/services";
 import CasCalendar from "../common/formfields/CasCalendar";
 import CasSelect from "../common/formfields/CasSelect";
+import CasTimePicker from "../common/formfields/CasTimePicker";
 
 class CasFormDemo extends Component {
-  constructor(props)
-  {
+  constructor(props) {
     super(props);
-    this.submitCancelForm=this.submitCancelForm.bind(this);
+    this.submitCancelForm = this.submitCancelForm.bind(this);
     this.setAirportsCallBack = this.setAirportsCallBack.bind(this);
-    this.state={
-      airportData :[]
-    }
+    this.state = {
+      airportData: [],
+    };
   }
   validateCancelForm(values) {
     const errors = {};
-    console.log(values)
+    console.log(values);
     if (values.cancelType === "") errors.cancelType = "Please choose the type";
     return errors;
   }
-  
+
   setAirportsCallBack(airportData) {
     this.setState({
-      airportData
+      airportData,
     });
   }
 
@@ -49,188 +49,237 @@ class CasFormDemo extends Component {
       cancelType: "",
     };
 
-    let carrierCodeOptions= [{
-      option:"test1"
-    },{option: "test2"}]
+    let carrierCodeOptions = [
+      {
+        option: "test1",
+      },
+      { option: "test2" },
+    ];
+
+    let cancelReasonOptions = [
+      {
+        option: "test1",
+      },
+      { option: "test2" },
+    ];
 
     return (
-             <>
-              <h1>{label.title}</h1>
+      <>
+        <h1>{label.title}</h1>
 
-              <Formik
-                initialValues={setInitialValues}
-                validate={this.validateCancelForm}
-                onSubmit={this.submitCancelForm}
-              >
-                {({
-                  values,
-                  errors,
-                  touched,
-                  handleChange,
-                  handleBlur,
-                  handleSubmit,
-                  isSubmitting,
-                  /* and other goodies */
-                }) => (
-                  <form onSubmit={handleSubmit}>
-                    {/* Row 1 */}
-                    <div className="p-grid">
-                      <div className="p-col-12 p-lg-2 p-md-2 p-field-radiobutton">
-                        <CasRadioBtn
-                          value="international"
-                          onChange={handleChange}
-                          name="cancelType"
-                          id="cancelIntFlight"
-                          checkedCondition={
-                            values.cancelType === "international"
-                          }
-                        />
-                        <label htmlFor="cancelIntFlight">
-                          {label.international}
-                        </label>
-                      </div>
+        <Formik
+          initialValues={setInitialValues}
+          validate={this.validateCancelForm}
+          onSubmit={this.submitCancelForm}
+          
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
 
-                      <div className="p-col-12 p-lg-2 p-md-2 p-field-radiobutton">
-                        <CasRadioBtn
-                          value="domestic"
-                          onChange={handleChange}
-                          name="cancelType"
-                          id="cancelDomFlight"
-                          checkedCondition={values.cancelType === "domestic"}
-                        />
-                        <label htmlFor="cancelDomFlight">
-                          {label.domestic}
-                        </label>
-                      </div>
-                      {errors.cancelType && (
-                        <small className="cas-inline-err-text">
-                          {errors.cancelType}
-                        </small>
-                      )}
-                    </div>
-                    {/* Row 2 */}
-                    <div className="p-grid">
-                      <div className="p-col-12 p-lg-2 p-md-2">
-                        <label htmlFor="cancelDepDate">{label.boardingStartDate}</label>
-                      </div>
-                      <div className="p-col-12  p-lg-2 p-md-2">
-                        <CasCalendar id="cancelDepDate" onChange={handleChange} name="depDate" showIcon={true} numberOfMonths={3}/>
-                      </div>
-                      <div className="p-col-12  p-lg-2 p-md-4"></div>
-                      <div className="p-col-12 p-lg-2 p-md-2">
-                        <label htmlFor="cancelArvDate">
-                          {label.boardingEndDate}
-                        </label>
-                      </div>
-                      <div className="p-col-12 p-lg-2 p-md-4">
-                      <CasCalendar id="cancelArvDate" onChange={handleChange} name="arvDate" showIcon={true} numberOfMonths={3}/>
-                      </div>
-                    </div>
+            /* and other goodies */
+          }) => (
+            <form onSubmit={handleSubmit}>
+              {/* Row 1 */}
+              <div className="p-grid">
+                <div className="p-col-12 p-lg-2 p-md-2 p-field-radiobutton">
+                  <CasRadioBtn
+                    value="international"
+                    onChange={handleChange}
+                    name="cancelType"
+                    id="cancelIntFlight"
+                    checkedCondition={values.cancelType === "international"}
+                  />
+                  <label htmlFor="cancelIntFlight">{label.international}</label>
+                </div>
 
-                    {/* Row 3 */}
-                    <div className="p-grid">
-                      <div className="p-col-12 p-lg-2 p-md-2">
-                        <label htmlFor="cancelDepAirport">
-                          {label.depAirport}
-                        </label>
-                      </div>
-                      <div className="p-col-12  p-lg-2 p-md-4">
-                        <CasAirportInput
-                          airportType="departure"
-                          id="cancelDepAirport"
-                          name="depAirport"
-                          airportData={this.state.airportData}
-                        />
-                      </div>
-                    </div>
-
-                    {/* Row 4 */}
-                    <div className="p-grid">
-                      <div className="p-col-12 p-lg-2 p-md-2">
-                        <label htmlFor="cancelArrAirport">
-                          {label.arrAirport}
-                        </label>
-                      </div>
-                      <div className="p-col-12 p-lg-2 p-md-4">
-                        <CasAirportInput
-                          airportType="arrival"
-                          id="cancelArrAirport"
-                          name="arvAirport"
-                          airportData={this.state.airportData}
-                        />
-                      </div>
-                    </div>
-
-                    {/* Row 5 */}
-                    <div className="p-grid">
-                      <div className="p-col-12 p-lg-2 p-md-2">
-                        <label htmlFor="cancelCarrierCD">{label.carrierCode}</label>
-                      </div>
-                      <div className="p-col-12  p-lg-4 p-md-4">
-                        <CasSelect id="cancelCarrierCD" name="carrierCode" options={carrierCodeOptions}
-                           value={values.carrierCode} onChange={handleChange}   
-                        />
-                      </div>
-                      <div className="p-col-12 p-lg-2 p-md-2">
-                        <label htmlFor="textarea">{label.flightNum}</label>
-                      </div>
-                      <div className="p-col-12 p-lg-4 p-md-4">
-                        <CasInputText id="cancelflightNumber" />
-                      </div>
-                    </div>
-
-                    {/* Row 6 */}
-                    <div className="p-grid">
-                      <div className="p-col-12 p-lg-2 p-md-2">
-                        <label htmlFor="input">{label.depTimeStart}</label>
-                      </div>
-                      <div className="p-col-12  p-lg-4 p-md-4">
-                        <CasInputText id="input" />
-                      </div>
-                      <div className="p-col-12 p-lg-2 p-md-2">
-                        <label htmlFor="textarea">{label.depTimeEnd}</label>
-                      </div>
-                      <div className="p-col-12 p-lg-4 p-md-4">
-                        <CasInputText id="textarea" />
-                      </div>
-                    </div>
-
-                    {/* Row 7 */}
-                    <div className="p-grid">
-                      <div className="p-col-12 p-lg-2 p-md-2">
-                        <label htmlFor="input">{label.ArvTimeStart}</label>
-                      </div>
-                      <div className="p-col-12  p-lg-4 p-md-4">
-                        <CasInputText id="input" />
-                      </div>
-                      <div className="p-col-12 p-lg-2 p-md-2">
-                        <label htmlFor="textarea">{label.ArvTimeEnd}</label>
-                      </div>
-                      <div className="p-col-12 p-lg-4 p-md-4">
-                        <CasInputText id="textarea" />
-                      </div>
-                    </div>
-
-                    {/* Row 8 */}
-                    <div className="p-grid">
-                      <div className="p-col-12 p-lg-2 p-md-2">
-                        <label htmlFor="input">{label.reason}</label>
-                      </div>
-                      <div className="p-col-12  p-lg-4 p-md-4">
-                        <CasInputText id="input" />
-                      </div>
-                    </div>
-
-                    <div className="p-grid">
-                      <div className="p-col-12 p-lg-9 p-md-9">&nbsp;</div>
-                      <div className="p-col-12 p-lg-2 p-md-2">
-                        <CasButton label="Search" disabled={isSubmitting} />
-                      </div>
-                    </div>
-                  </form>
+                <div className="p-col-12 p-lg-2 p-md-2 p-field-radiobutton">
+                  <CasRadioBtn
+                    value="domestic"
+                    onChange={handleChange}
+                    name="cancelType"
+                    id="cancelDomFlight"
+                    checkedCondition={values.cancelType === "domestic"}
+                  />
+                  <label htmlFor="cancelDomFlight">{label.domestic}</label>
+                </div>
+                {errors.cancelType && (
+                  <small className="cas-inline-err-text">
+                    {errors.cancelType}
+                  </small>
                 )}
-              </Formik>
-            </>
+              </div>
+              {/* Row 2 */}
+              <div className="p-grid">
+                <div className="p-col-12 p-lg-2 p-md-2">
+                  <label htmlFor="cancelDepDate">
+                    {label.boardingStartDate}
+                  </label>
+                </div>
+                <div className="p-col-12  p-lg-2 p-md-2">
+                  <CasCalendar
+                    id="cancelDepDate"
+                    onChange={handleChange}
+                    name="depDate"
+                    showIcon={true}
+                    numberOfMonths={3}
+                  />
+                </div>
+                <div className="p-col-12  p-lg-2 p-md-4"></div>
+                <div className="p-col-12 p-lg-2 p-md-2">
+                  <label htmlFor="cancelArvDate">{label.boardingEndDate}</label>
+                </div>
+                <div className="p-col-12 p-lg-2 p-md-4">
+                  <CasCalendar
+                    id="cancelArvDate"
+                    onChange={handleChange}
+                    name="arvDate"
+                    showIcon={true}
+                    numberOfMonths={3}
+                  />
+                </div>
+              </div>
+
+              {/* Row 3 */}
+              <div className="p-grid">
+                <div className="p-col-12 p-lg-2 p-md-2">
+                  <label htmlFor="cancelDepAirport">{label.depAirport}</label>
+                </div>
+                <div className="p-col-12  p-lg-2 p-md-4">
+                  <CasAirportInput
+                    airportType="departure"
+                    id="cancelDepAirport"
+                    name="depAirport"
+                    airportData={this.state.airportData}
+                  />
+                </div>
+              </div>
+
+              {/* Row 4 */}
+              <div className="p-grid">
+                <div className="p-col-12 p-lg-2 p-md-2">
+                  <label htmlFor="cancelArrAirport">{label.arrAirport}</label>
+                </div>
+                <div className="p-col-12 p-lg-2 p-md-4">
+                  <CasAirportInput
+                    airportType="arrival"
+                    id="cancelArrAirport"
+                    name="arvAirport"
+                    airportData={this.state.airportData}
+                  />
+                </div>
+              </div>
+
+              {/* Row 5 */}
+              <div className="p-grid">
+                <div className="p-col-12 p-lg-2 p-md-2">
+                  <label htmlFor="cancelCarrierCD">{label.carrierCode}</label>
+                </div>
+                <div className="p-col-12  p-lg-4 p-md-4">
+                  <CasSelect
+                    id="cancelCarrierCD"
+                    name="carrierCode"
+                    options={carrierCodeOptions}
+                    value={values.carrierCode}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="p-col-12 p-lg-2 p-md-2">
+                  <label htmlFor="cancelflightNumber">{label.flightNum}</label>
+                </div>
+                <div className="p-col-12 p-lg-4 p-md-4">
+                  <CasInputText
+                    value={values.flightNumber}
+                    id="cancelflightNumber"
+                    name="flightNumber"
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+
+              {/* Row 6 */}
+              <div className="p-grid">
+                <div className="p-col-12 p-lg-2 p-md-2">
+                  <label htmlFor="cancelDepTimeStart">{label.depTimeStart}</label>
+                </div>
+                <div className="p-col-12  p-lg-4 p-md-4">
+                  <CasTimePicker
+                    id="cancelDepTimeStart"
+                    onChange={handleChange}
+                    name="depTimeStart"
+                    value={values.depTimeStart}
+                  />
+                </div>
+                <div className="p-col-12 p-lg-2 p-md-2">
+                  <label htmlFor="cancelDepTimeEnd">{label.depTimeEnd}</label>
+                </div>
+                <div className="p-col-12 p-lg-4 p-md-4">
+                <CasTimePicker
+                    id="cancelDepTimeEnd"
+                    onChange={handleChange}
+                    name="depTimeEnd"
+                    value={values.depTimeEnd}
+                  />
+                </div>
+              </div>
+
+              {/* Row 7 */}
+              <div className="p-grid">
+                <div className="p-col-12 p-lg-2 p-md-2">
+                  <label htmlFor="cancelArvTimeStart">{label.ArvTimeStart}</label>
+                </div>
+                <div className="p-col-12  p-lg-4 p-md-4">
+                <CasTimePicker
+                    id="cancelArvTimeStart"
+                    onChange={handleChange}
+                    name="arvTimeStart"
+                    value={values.arvTimeStart}
+                  />
+                </div>
+                <div className="p-col-12 p-lg-2 p-md-2">
+                  <label htmlFor="cancelArvTimeEnd">{label.ArvTimeEnd}</label>
+                </div>
+                <div className="p-col-12 p-lg-4 p-md-4">
+                <CasTimePicker
+                    id="cancelArvTimeEnd"
+                    onChange={handleChange}
+                    name="arvTimeEnd"
+                    value={values.arvTimeEnd}
+                  />
+                </div>
+              </div>
+
+              {/* Row 8 */}
+              <div className="p-grid">
+                <div className="p-col-12 p-lg-2 p-md-2">
+                  <label htmlFor="input">{label.reason}</label>
+                </div>
+                <div className="p-col-12  p-lg-4 p-md-4">
+                  <CasSelect
+                    id="cancelReason"
+                    name="reason"
+                    options={cancelReasonOptions}
+                    value={values.reason}
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+
+              <div className="p-grid">
+                <div className="p-col-12 p-lg-9 p-md-9">&nbsp;</div>
+                <div className="p-col-12 p-lg-2 p-md-2">
+                  <CasButton label="Search" disabled={isSubmitting} />
+                </div>
+              </div>
+            </form>
+          )}
+        </Formik>
+      </>
     );
   }
 }
