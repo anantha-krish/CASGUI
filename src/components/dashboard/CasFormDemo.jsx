f2e92378067f2d3de6f7ca1eb0fd1132186cea6d
Anantha Krishnan <anantha.krishnan@ibsplc.com>
2020-10-22 11:17:48 +0530

    Added Label constants Updated Labels in CasFormDemo By Anantha

--- src/components/dashboard/CasFormDemo.jsx
+++ src/components/dashboard/CasFormDemo.jsx
@@ -1,41 +1,38 @@
 import React, { Component } from "react";
+import { LabelConstants } from "../../constants/constants";
 import CasButton from "../common/formfields/CasButton";
 import CasInputText from "../common/formfields/CasInputText";
 
 export class CasFormDemo extends Component {
   render() {
+    let label = LabelConstants.cancelFormPage;
+    let errTest = "Lorem ipsum dolor sit amet consectetur, adipisicing elit.";
     return (
       <div className="p-fluid">
         <div className="p-grid">
           <div className="p-col-12">
             <div className="card card-w-title">
-              <h1>Cas Form Demo</h1>
+              <h1>{label.title}</h1>
               {/* Row 1 */}
               <div className="p-grid">
                 <div className="p-col-12 p-lg-2 p-md-2">
-                  <label htmlFor="input">Input</label>
+                  <label htmlFor="input">{label.region}</label>
                 </div>
                 <div className="p-col-12  p-lg-4 p-md-4">
-                  <CasInputText id="input" />
-                </div>
-                <div className="p-col-12 p-lg-2 p-md-2">
-                  <label htmlFor="textarea">Textarea</label>
-                </div>
-                <div className="p-col-12 p-lg-4 p-md-4">
-                  <CasInputText id="textarea" />
+                  <CasInputText errorText={errTest} id="input" />
                 </div>
               </div>
 
               {/* Row 2 */}
               <div className="p-grid">
                 <div className="p-col-12 p-lg-2 p-md-2">
-                  <label htmlFor="input">Input</label>
+                  <label htmlFor="input">{label.boardingStartDate}</label>
                 </div>
                 <div className="p-col-12  p-lg-4 p-md-4">
                   <CasInputText id="input" />
                 </div>
                 <div className="p-col-12 p-lg-2 p-md-2">
-                  <label htmlFor="textarea">Textarea</label>
+                  <label htmlFor="textarea">{label.boardingEndDate}</label>
                 </div>
                 <div className="p-col-12 p-lg-4 p-md-4">
                   <CasInputText id="textarea" />
@@ -45,29 +42,17 @@ export class CasFormDemo extends Component {
               {/* Row 3 */}
               <div className="p-grid">
                 <div className="p-col-12 p-lg-2 p-md-2">
-                  <label htmlFor="input">Input</label>
+                  <label htmlFor="input">{label.depAirport}</label>
                 </div>
                 <div className="p-col-12  p-lg-4 p-md-4">
                   <CasInputText id="input" />
-                </div>
-                <div className="p-col-12 p-lg-2 p-md-2">
-                  <label htmlFor="textarea">Textarea</label>
-                </div>
-                <div className="p-col-12 p-lg-4 p-md-4">
-                  <CasInputText id="textarea" />
                 </div>
               </div>
 
               {/* Row 4 */}
               <div className="p-grid">
                 <div className="p-col-12 p-lg-2 p-md-2">
-                  <label htmlFor="input">Input</label>
-                </div>
-                <div className="p-col-12  p-lg-4 p-md-4">
-                  <CasInputText id="input" />
-                </div>
-                <div className="p-col-12 p-lg-2 p-md-2">
-                  <label htmlFor="textarea">Textarea</label>
+                  <label htmlFor="textarea">{label.arrAirport}</label>
                 </div>
                 <div className="p-col-12 p-lg-4 p-md-4">
                   <CasInputText id="textarea" />
@@ -77,13 +62,13 @@ export class CasFormDemo extends Component {
               {/* Row 5 */}
               <div className="p-grid">
                 <div className="p-col-12 p-lg-2 p-md-2">
-                  <label htmlFor="input">Input</label>
+                  <label htmlFor="input">{label.carrierCode}</label>
                 </div>
                 <div className="p-col-12  p-lg-4 p-md-4">
                   <CasInputText id="input" />
                 </div>
                 <div className="p-col-12 p-lg-2 p-md-2">
-                  <label htmlFor="textarea">Textarea</label>
+                  <label htmlFor="textarea">{label.flightNum}</label>
                 </div>
                 <div className="p-col-12 p-lg-4 p-md-4">
                   <CasInputText id="textarea" />
@@ -93,13 +78,13 @@ export class CasFormDemo extends Component {
               {/* Row 6 */}
               <div className="p-grid">
                 <div className="p-col-12 p-lg-2 p-md-2">
-                  <label htmlFor="input">Input</label>
+                  <label htmlFor="input">{label.depTimeStart}</label>
                 </div>
                 <div className="p-col-12  p-lg-4 p-md-4">
                   <CasInputText id="input" />
                 </div>
                 <div className="p-col-12 p-lg-2 p-md-2">
-                  <label htmlFor="textarea">Textarea</label>
+                  <label htmlFor="textarea">{label.depTimeEnd}</label>
                 </div>
                 <div className="p-col-12 p-lg-4 p-md-4">
                   <CasInputText id="textarea" />
@@ -109,13 +94,13 @@ export class CasFormDemo extends Component {
               {/* Row 7 */}
               <div className="p-grid">
                 <div className="p-col-12 p-lg-2 p-md-2">
-                  <label htmlFor="input">Input</label>
+                  <label htmlFor="input">{label.ArvTimeStart}</label>
                 </div>
                 <div className="p-col-12  p-lg-4 p-md-4">
                   <CasInputText id="input" />
                 </div>
                 <div className="p-col-12 p-lg-2 p-md-2">
-                  <label htmlFor="textarea">Textarea</label>
+                  <label htmlFor="textarea">{label.ArvTimeEnd}</label>
                 </div>
                 <div className="p-col-12 p-lg-4 p-md-4">
                   <CasInputText id="textarea" />
@@ -125,23 +110,15 @@ export class CasFormDemo extends Component {
               {/* Row 8 */}
               <div className="p-grid">
                 <div className="p-col-12 p-lg-2 p-md-2">
-                  <label htmlFor="input">Input</label>
+                  <label htmlFor="input">{label.reason}</label>
                 </div>
                 <div className="p-col-12  p-lg-4 p-md-4">
                   <CasInputText id="input" />
                 </div>
-                <div className="p-col-12 p-lg-2 p-md-2">
-                  <label htmlFor="textarea">Textarea</label>
-                </div>
-                <div className="p-col-12 p-lg-4 p-md-4">
-                  <CasInputText id="textarea" />
-                </div>
               </div>
 
               <div className="p-grid">
-                <div className="p-col">&nbsp;</div>
-              </div>
-              <div className="p-grid">
+                <div className="p-col-12 p-lg-9 p-md-9">&nbsp;</div>
                 <div className="p-col-12 p-lg-2 p-md-2">
                   <CasButton label="Search" />
                 </div>
---
+++ src/constants/LabelConstants.js
@@ -0,0 +1,18 @@
+export const LabelConstants ={
+    cancelFormPage :{
+        title: "Registration and reference of cancellation reasons",
+        region:"International / domestic",
+        boardingStartDate: "Boarding start date",
+        boardingEndDate: "Boarding end date",
+        depAirport : "Departure airport",
+        arrAirport: "Arrival airport",
+        carrierCode: "Carrier code",
+        flightNum : "Flight number",
+        depTimeStart: "Departure time start",
+        depTimeEnd: "Departure time end",
+        ArvTimeStart:"Arrival time start",
+        ArvTimeEnd:"Arrival time end",
+        reason: "Reason for cancellation"
+
+    }
+}
