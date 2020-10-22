942f07c6f5636c7380e7773522076494a4e0aff4
MarvelPOC <68634759+MarvelPOC@users.noreply.github.com>
2020-10-22 09:14:50 +0530

    Update CasApp.jsx

--- src/CasApp.jsx
+++ src/CasApp.jsx
@@ -6,6 +6,10 @@ import CasHomeScreen from "./components/home/CasHomeScreen";
 import "primereact/resources/themes/nova/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faPlane,faPlaneArrival,faPlaneDeparture } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faPlane, faPlaneArrival,faPlaneDeparture);
 
 class CasApp extends Component {
   
