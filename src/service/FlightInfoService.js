f61af9ca882fc11d1ce838b0353bdce97ab3118a
MarvelPOC <68634759+MarvelPOC@users.noreply.github.com>
2020-10-23 18:25:55 +0530

    Update FlightInfoService.js

--- src/service/FlightInfoService.js
+++ src/service/FlightInfoService.js
@@ -3,6 +3,7 @@ import { ErrorConstants } from "../constants/constants";
 
 export const FlightInfoService = {
   createNewFlightInfo,
+  getAllFlightInfo
 };
 
 function createNewFlightInfo(data) {
@@ -15,6 +16,25 @@ function createNewFlightInfo(data) {
     .then((res) => {
       if (res.location) {
           return Promise.resolve(true)
+      } else {
+        let error = [ErrorConstants.CAS_GUI_ERR_01];
+        return Promise.reject(error);
+      }
+    })
+    .catch((err) => {
+      return Promise.reject([ErrorConstants.CAS_GUI_ERR_01]);
+    });
+}
+
+
+function getAllFlightInfo(callbackFn) {
+  return axios
+    .get("http://localhost:8080/cas-gui/flight-info")
+    .then((res) => {
+      if (res.data && res.data.length) {
+        if (callbackFn) {
+          callbackFn(res.data);
+        }
       } else {
         let error = [ErrorConstants.CAS_GUI_ERR_01];
         return Promise.reject(error);
